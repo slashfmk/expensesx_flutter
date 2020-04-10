8636772d2f22a0db2ef5061266b353e637683ba3
Yannick Fumukani <slashcs7@hotmail.com>
2020-04-10 05:21:25 -0500

    Fix notification bar color bug

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.white, //or set color with: Color(0xFF0000FF)
+      statusBarColor: kDarkBlue, //or set color with: Color(0xFF0000FF)
     ));
     return MaterialApp(
       title: 'Expenses x',
       theme: ThemeData.dark().copyWith(
-       // primarySwatch: Colors.blue,
+        // primarySwatch: Colors.blue,
         primaryColor: kDarkBlue,
         accentColor: kSecondary, // color for forms and others
         scaffoldBackgroundColor: kDarkBlue,
-        textTheme: TextTheme(body1: TextStyle(color: Colors.white, fontSize: 12)),
+        textTheme:
+            TextTheme(body1: TextStyle(color: Colors.white, fontSize: 12)),
       ),
       initialRoute: '/signIn',
       routes: {
